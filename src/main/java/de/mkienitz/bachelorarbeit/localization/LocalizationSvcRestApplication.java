4d681f24fe79f6a17a4c9c49910a97635d9ed0fb
JapuDCret <JapuDCret@users.noreply.github.com>
2021-01-12 10:48:19 +0100

    small changes

--- src/main/java/de/mkienitz/bachelorarbeit/localization/LocalizationSvcRestApplication.java
+++ src/main/java/de/mkienitz/bachelorarbeit/localization/LocalizationSvcRestApplication.java
@@ -2,10 +2,20 @@ package de.mkienitz.bachelorarbeit.localization;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  */
 @ApplicationPath("/data")
 public class LocalizationSvcRestApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+
+        s.add(TranslationResource.class);
+
+        return s;
+    }
 }
